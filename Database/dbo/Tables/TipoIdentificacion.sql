b942bd3963d92acdf5c8a9f5eda907f1e8ecdb81
giovannisv <geoseve@hotmail.com>
2021-10-07 23:07:33 -0600

    Actualizatodotarea1

--- Database/dbo/Tables/TipoIdentificacion.sql
+++ Database/dbo/Tables/TipoIdentificacion.sql
@@ -1,7 +1,11 @@
 ﻿CREATE TABLE [dbo].[TipoIdentificacion]
 (
-	IdTipoIdentificacion INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Empleado PRIMARY KEY CLUSTERED(IdTipoIdentificacion)
-  , Nombre VARCHAR(250) NOT NULL 
+	
+	IdCedula INT NOT NULL  IDENTITY(1,1) CONSTRAINT PK_TipoIdentificacion PRIMARY KEY CLUSTERED(IdCedula)
+   ,IdTipoIdentificacion INT NOT NULL
+  ,Pasaporte VARCHAR(250) NOT NULL
+  ,CedulaJuridica VARCHAR (250) NOT NULL
+  ,Descripcion VARCHAR (250) NOT NULL
   
 )
 WITH (DATA_COMPRESSION = PAGE)
---
+++ InitialData/ScriptConfiguration/04-TipoIdentificacion.sql
@@ -0,0 +1,31 @@
+﻿DROP TABLE IF EXISTS #TipoIdentificacionTemp
+
+SELECT 
+IdCedula,Pasaporte,CedulaJuridica  INTO #TipoIdentificacionTemp
+FROM (
+VALUES
+(1, 'RE-13540','OP500'),
+(2, 'AL-14545','AP550'),
+(3, 'SA-3543','SP560')
+
+)AS TEMP (IdCedula,Pasaporte,CedulaJuridica)
+
+
+----ACTUALIZAR DATOS---
+UPDATE P SET
+  P.Pasaporte= TM.Pasaporte
+  P.CedulaJuridica= TM.CedulaJuridica
+FROM dbo.TipoIdentificacion P
+INNER JOIN #TipoIdentificacionTemp TM
+    ON P.IdCedula= TM.IdCedula
+
+
+----INSERTAR DATOS---
+
+SET IDENTITY_INSERT dbo.TipoIdentificacion ON
+
+INSERT INTO dbo.TipoIdentificacion(
+IdCedula,Pasaporte,CedulaJuridica)
+SELECT
+IdCedula,Pasaporte,CedulaJuridica
+FROM #TipoIdentificacionTemp
